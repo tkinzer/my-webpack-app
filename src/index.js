e9a31461a1309ac90c9a73993a5dd287bbe74e80
Tim Kinzer <timothykinzer@gmail.com>
2018-12-04 11:14:13 -0800

    Basic React Template with Webpack and Babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
-document.addEventListener("DOMContentLoaded", function(event) {
-    const element = document.createElement('h1')
-    element.innerHTML = "Hello World"
-    document.body.appendChild(element)
-  })
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+ReactDOM.render(
+  <App />, document.getElementById('root'));
